58ee263fa5866e639f7537e2a339fea339595e97
jikuya <jikuya@cv.info.gifu-u.ac.jp>
2022-10-18 10:55:22 +0000

    Update setup.sh nvidia driver

--- setup.sh
+++ setup.sh
@@ -46,9 +46,7 @@ sudo pkill -SIGHUP dockerd
 
 # NVIDIAドライバ インストール
 
-sudo apt-get install linux-headers-$(uname -r)
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update -y
-sudo apt-get -y install cuda-drivers
+sudo apt-get update
+sudo apt-get -y install cuda
