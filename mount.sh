404298309e62d633a79f2154a1270619ed67bd30
jikuya <jikuya@cv.info.gifu-u.ac.jp>
2022-10-24 19:10:38 +0900

    versオプションつけました

--- mount.sh
+++ mount.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y nfs-common
 
 sudo mkdir -p /mnt/gpu-node
-sudo sh -c 'echo "10.226.47.91:/home /mnt/gpu-node nfs rw 0 0" >> /etc/fstab'
+sudo sh -c 'echo "10.226.47.91:/home /mnt/gpu-node nfs rw,vers=3 0 0" >> /etc/fstab'
 sudo mkdir /mnt/qnap2
 sudo sh -c 'echo "10.226.47.81:/homes /mnt/qnap2 nfs rw 0 0" >> /etc/fstab'
 sudo mount -a
